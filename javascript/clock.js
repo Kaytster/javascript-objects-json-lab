3aef3764d45dbc504c838f17e07effe6210fa098
Sharp <c4055518@hallam.shu.ac.uk>
2024-11-13 10:47:30 +0000

    added a staff table

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,13 +2,29 @@
 //*start
 console.info("Hello JS");
 const myNode = document.createElement("div");
+
+//*Digital Clock
 const digitalClock = document.getElementById("digitalClock");
 digitalClock.appendChild(myNode);
 
-const myDate = new Date();
-console.dir(myDate);
+//console.dir(myDate);
 // *show only hours, minutes and seconds
-myNode.innerHTML = myDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" });
 //myNode.innerHTML = myDate.toDateString(); 
+
+const updateTime = () => {
+
+console.info('tick tock');
+
+const myDate = new Date();
+myNode.innerHTML = myDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+}
+
+setInterval(updateTime, 1000);
+
+//*Analogue Clock
+  //const secondHand = document.getElementById("secondHand");
+  //const minHand = document.getElementById("minHand");
+  //const hrHand = document.getElementById("hrHand");
+  //hrHand.style.backgroundColor = '#0f0';
 //*end
 })();
